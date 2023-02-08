dffc8401e1f31192cc17fd585cb2e003d149bbaf
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-02-08 11:07:54 -0800

    models/User.js file is finished

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # NoSQL_Social_Network_API
+
+A social media plathform using an API for my social network that uses a NoSQL database
+so, my website can handle large amounts of unstructured data
+
+## Description
+
+GIVEN a social network API
+WHEN I enter the command to invoke the application
+THEN my server is started and the Mongoose models are synced to the MongoDB database
+WHEN I open API GET routes in Insomnia for users and thoughts
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia
+THEN I am able to successfully create, update, and delete users and thoughts in my database
+WHEN I test API POST and DELETE routes in Insomnia
+THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
+
+## Usage
+
+User can join community to share thoughts and leave reaction
+
+## Screenshot
+
+
+
+## Link to demonstaration video
+
+
+
+## Credits
+
+N/A
+
+## License
+
+MIT License according to the LICENSE file in the repo.
+
+## Resources
+- https://mongoosejs.com/docs/schematypes.html
+- https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+- https://mongoosejs.com/docs/validation.html
+- https://stackoverflow.com/questions/22244421/how-to-create-mongoose-schema-with-array-of-object-ids
+- 
+
---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+const reactionSchema = require('./Reaction');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        //??? or to use validate ???
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    // ??? Array of _id values referencing the Thought model ???
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    // ??? Array of _id values referencing the User model (self-reference) ???
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// ??? virtual called friendCount that retrieves the length of the user's friends array field on query ???
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    // ??? string or number this.friends.length ???
+    return `${this.friends.length}`;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
